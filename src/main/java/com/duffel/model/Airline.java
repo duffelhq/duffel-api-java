2724921700c902d875ec1312eab02b35b3f3aced
John Peter Harvey <john@duffel.com>
2022-07-14 09:06:01 +0100

    Airline logo support

--- src/main/java/com/duffel/model/Airline.java
+++ src/main/java/com/duffel/model/Airline.java
@@ -23,6 +23,21 @@ public class Airline extends Data<Airline> {
     private String id;
 
     ///  <summary>
+    ///  Path to a svg of the airline lockup logo. A lockup logo is also called a combination logo, in which it combines
+    ///  the logotype and logomark. This may be null if no logo is available.
+    ///  Example: "https://assets.duffel.com/img/airlines/for-light-background/full-color-lockup/BA.svg"
+    ///  </summary>
+    @JsonProperty("logo_lockup_url")
+    private String logoLookupUrl;
+
+    ///  <summary>
+    ///  Path to a svg of the airline logo. This may be null if no logo is available.
+    ///  Example: "https://assets.duffel.com/img/airlines/for-light-background/full-color-logo/BA.svg"
+    ///  </summary>
+    @JsonProperty("logo_symbol_url")
+    private String logoSymbolUrl;
+
+    ///  <summary>
     ///  The name of the airline
     ///  </summary>
     @JsonProperty("name")
