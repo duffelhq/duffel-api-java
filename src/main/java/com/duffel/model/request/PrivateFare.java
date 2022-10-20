b1075affb4a5514efb6dd03822ea6c1bd53a20c2
John Peter Harvey <john@duffel.com>
2022-10-20 11:56:30 +0100

    Add some quick javadocs

--- src/main/java/com/duffel/model/request/PrivateFare.java
+++ src/main/java/com/duffel/model/request/PrivateFare.java
@@ -6,15 +6,25 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+/**
+ * Private fare code, for retrieving agreed custom fares and providing business codes.
+ */
 @EqualsAndHashCode
 @Getter
 @Setter
 @ToString
 public class PrivateFare {
 
+    /**
+     * Airline provided corporate code.
+     */
     @JsonProperty("corporate_code")
     private String corporateCode;
 
+    /**
+     * Reference to identify the business
+     * e.g. ABN:2345678
+     */
     @JsonProperty("tracking_reference")
     private String trackingReference;
 
