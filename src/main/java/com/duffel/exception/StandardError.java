3e07953d8767317313aad7e5e5200d2a285907d4
John Peter Harvey <john@duffel.com>
2022-12-05 15:50:53 +0000

    Also StandardError

--- src/main/java/com/duffel/exception/StandardError.java
+++ src/main/java/com/duffel/exception/StandardError.java
@@ -10,7 +10,7 @@ import lombok.ToString;
  */
 @EqualsAndHashCode(callSuper = true)
 @Getter
-@ToString
+@ToString(callSuper = true)
 public class StandardError extends Error {
 
     @JsonProperty("source")
